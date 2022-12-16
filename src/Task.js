eb1fde460437df1fa324272514368d51328f71e6
arghyyy <argisilaen@gmail.com>
2022-12-16 17:48:38 +0700

    confirm

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "assignment-frontenddeployment.netlify.app" // TODO: replace this
 const studentName = "YOURNAME" // TODO: replace this
 const studentId = "YOURSTUDENTID" // TODO: replace this
 
