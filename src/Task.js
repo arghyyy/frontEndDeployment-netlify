29625e0c6a21a9c3383f5a7786b54ddff93864e0
arghyyy <argisilaen@gmail.com>
2022-12-16 17:49:42 +0700

    confirm

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "assignment-frontenddeployment.netlify.app" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const studentName = "ABRAHAM ARGHYA S" // TODO: replace this
+const studentId = "FE2660469" // TODO: replace this
 
 export { url, studentName, studentId }
