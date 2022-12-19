a45fe78a5b3ccc78c337b558bb55a3583e17da0d
arghyyy <argisilaen@gmail.com>
2022-12-19 15:49:30 +0700

    confirm

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <div className='navbar'>
         <div className='studentInfo'>
-          <h2 className='studentName'>Abraham Arghya S</h2>
+          <h2 className='studentName'>ABRAHAM ARGHYA S</h2>
           <p className='studentId'>FE2660469</p>
         </div>
         <Link to='/'>Home</Link>
